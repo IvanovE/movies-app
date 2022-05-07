f91fa8a3c74f7d1534583e984eb5eacd1289cde6
IvanovE <EugeneIvanov12@gmail.com>
2022-05-07 18:15:34 +0300

    add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 export function App() {
-	return (
-		<div>
-            Working!
-		</div>
-	);
+  return (
+    <div>
+        Working!
+    </div>
+  );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { App } from './App';
@@ -8,9 +9,9 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>
 );
 
 reportWebVitals();
