60440c89cda45345ea22b5f510b34c6077521c95
IvanovE <EugeneIvanov12@gmail.com>
2022-06-12 16:09:26 +0300

    implement movies api

---
+++ src/store/api/transformAdapter.ts
@@ -0,0 +1,77 @@
+import {
+  ISingleMovieResponse,
+  IMoviesGroupResponse,
+  IMovieReviewsResponse
+} from '../../types/responses';
+import {
+  IStandardMovie,
+  IReview
+} from '../../types/common';
+
+const standardMovieTransform = (movie: IStandardMovie) => {
+  return {
+    id: movie.id,
+    adult: movie.adult,
+    poster: movie.poster_path,
+    backdrop: movie.backdrop_path,
+    overview: movie.overview,
+    title: movie.title,
+    release: movie.release_date,
+    rating: movie.vote_average + ' / 10'
+  };
+};
+
+const reviewTransform = (review: IReview) => {
+  return {
+    author: review.author,
+    name: review.author_details.name,
+    username: review.author_details.username,
+    avatar: review.author_details.avatar_path,
+    rating: review.author_details.rating,
+    content: review.content,
+    createdAt: review.created_at,
+    id: review.id
+  };
+};
+
+export const transformGetSingleMovie = (response: ISingleMovieResponse) => {
+  return {
+    id: response.id,
+    poster: response.poster_path,
+    backdrop: response.backdrop_path,
+    adult: response.adult,
+    overview: response.overview,
+    title: response.title,
+    status: response.status,
+    tagline: response.tagline,
+    release: response.release_date,
+    rating: response.vote_average + ' / 10',
+    runtime: response.runtime ? response.runtime + ' min' : null,
+    budget: response.budget + '$',
+    revenue: response.revenue + '$',
+    genres: response.genres.map((genre) => genre.name).join(', '),
+    companies: response.production_companies.map((company) => company.name).join(', '),
+    countries: response.production_countries.map((country) => country.name).join(', ')
+  };
+};
+
+export const transformMoviesGroup = (response: IMoviesGroupResponse) => {
+  const totalPages = response.total_pages;
+  const totalResults = response.total_results;
+  const results = response.results.map(standardMovieTransform);
+
+  return {
+    totalPages,
+    totalResults,
+    results
+  };
+};
+
+export const transformReviews = (response: IMovieReviewsResponse) => {
+  const totalResults = response.total_results;
+  const reviews = response.results.map(reviewTransform);
+  return {
+    totalResults,
+    results: reviews
+  };
+};
--- src/types/responses.ts
+++ src/types/responses.ts
@@ -1,7 +1,44 @@
-import { IUsers } from './common';
+import {
+  IUsers,
+  IGenre,
+  ICompany,
+  ICountry,
+  IReview,
+  IStandardMovie
+} from './common';
 
 export interface IAuthResponse {
   users: IUsers
   isSignedIn: boolean
   currentUser: string | null
+}
+
+export interface ISingleMovieResponse {
+  id: number
+  adult: boolean
+  backdrop_path: string | null
+  poster_path: string | null
+  overview: string | null
+  status: string
+  tagline: string | null
+  title: string
+  release_date: string
+  runtime: number | null
+  budget: number
+  revenue: number
+  vote_average: number
+  genres: IGenre[]
+  production_companies: ICompany[]
+  production_countries: ICountry[]
+}
+
+export interface IMoviesGroupResponse {
+  results: IStandardMovie[]
+  total_pages: number
+  total_results: number
+}
+
+export interface IMovieReviewsResponse {
+  results: IReview[]
+  total_results: number
 }
