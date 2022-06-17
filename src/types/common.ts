ef879127dc85422e746f03ae21c9cf8d1e10b7bd
IvanovE <53431008+IvanovE@users.noreply.github.com>
2022-06-17 19:24:06 +0300

    implement movies api (#5)

--- src/types/common.ts
+++ src/types/common.ts
@@ -4,6 +4,47 @@ export interface IUser {
   username: string
 }
 
+export interface IGenre {
+  id: number
+  name: string
+}
+
+export interface ICountry {
+  iso_3166_1: string
+  name: string
+}
+
+export interface ICompany {
+  id: number,
+  logo_path: string | null,
+  name: string,
+  origin_country: string
+}
+
+export interface IStandardMovie {
+  id: number
+  adult: boolean
+  backdrop_path: string | null
+  poster_path: string | null
+  overview: string | null
+  title: string
+  release_date: string
+  vote_average: number
+}
+
+export interface IReview {
+  author: string
+  author_details: {
+    name: string
+    username: string
+    avatar_path: string | null
+    rating: number | null
+  }
+  content: string
+  created_at: string
+  id: string
+}
+
 export interface IUsers {
   [key: string]: IUser
 }
---
+++ src/types/transforms.ts
@@ -0,0 +1,51 @@
+export interface ITransformedMovieDetails {
+  id: number
+  poster: string | null
+  backdrop: string | null
+  adult: boolean
+  overview: string | null
+  title: string
+  status: string
+  tagline: string | null
+  release: string
+  rating: string
+  runtime: string | null
+  budget: string
+  revenue: string
+  genres: string
+  companies: string
+  countries: string
+}
+
+interface ITransformedStandardMovie {
+  id: number
+  adult: boolean
+  backdrop: string | null
+  poster: string | null
+  overview: string | null
+  title: string
+  release: string
+  rating: string
+}
+
+export interface ITransformedMoviesGroup {
+  results: ITransformedStandardMovie[]
+  totalPages: number
+  totalResults: number
+}
+
+interface ITransformedReview {
+  author: string
+  name: string
+  username: string
+  avatar: string | null
+  rating: number | null
+  content: string
+  createdAt: string
+  id: string
+}
+
+export interface ITransformedReviews {
+  results: ITransformedReview[]
+  totalResults: number
+}
